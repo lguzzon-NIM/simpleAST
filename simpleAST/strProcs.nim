142b38c5914d4b83441b059e065c8fd229f591a5
Luca Guzzon <lucaguzzon@gmail.com>
2021-11-05 00:35:30 +0100

    Little optimization ...

--- simpleAST/strProcs.nim
+++ simpleAST/strProcs.nim
@@ -3,7 +3,7 @@ import setProcs
 
 
 proc strToEscapedStr* (aString: string, aEscapeChar: char, aEscapeCharSet:set[char]): string {. inline .} =
-  result = ""
+  result = newStringOfCap((aString.len * 125) div 100)
   let lEscapeCharSet = aEscapeCharSet + {aEscapeChar}
   for lChar in aString:
     if lChar in lEscapeCharSet:
@@ -19,9 +19,9 @@ proc strToEscapedStr* (aString, aEscapeChars: string): string {. inline .} =
 
 
 proc escapedStrToStr* (aString: string, aEscapeChar: char): string {. inline .} =
-  result = ""
-  var lIndex = 0
   let lLen = aString.len
+  result = newStringOfCap(lLen)
+  var lIndex = 0
   while lIndex < lLen:
     if aString[lIndex] == aEscapeChar:
       lIndex.inc
